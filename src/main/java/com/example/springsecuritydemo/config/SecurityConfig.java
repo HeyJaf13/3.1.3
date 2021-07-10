5ec40fb7069f28075906520dc09690db3b697493
Stanislav Dusiak <stanislav.dusiak@gmail.com>
2021-07-10 16:06:30 +0300

    done2

--- src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.springsecuritydemo.config;
 
 import com.example.springsecuritydemo.config.Handler.LoginSuccessHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
